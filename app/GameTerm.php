5a94ecd5455b9901921d25675a5c4328df4e10e9
umair321 <P@werfull1>
2017-04-01 19:56:55 +0500

    Reverted changes

--- app/GameTerm.php
+++ app/GameTerm.php
@@ -8,11 +8,15 @@ class GameTerm extends Model {
 
     protected $table = 'game_terms';
     protected $fillable = [
-        'name', 'game_id'
+        'name', 'game_id', 'game_action_id'
     ];
 
     public function game() {
         return $this->belongsToMany('App\Game');
+    }
+
+    public function game_actions() {
+        return $this->belongsTo('App\GameAction', 'game_action_id');
     }
 
 }

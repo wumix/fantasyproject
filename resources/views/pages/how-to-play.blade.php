0e09ba8c2c88a9ac3aaab53654fa047213c28ce9
umair321 <P@werfull1>
2017-04-06 02:52:17 +0500

    Player category on tournament

--- resources/views/pages/how-to-play.blade.php
+++ resources/views/pages/how-to-play.blade.php
@@ -70,7 +70,10 @@
                                                         @foreach($termFromToPoints as $termPointIndex => $termPointVal)
                                                         <tr  class="cwt">
                                                             <td>
-                                                                @php($fromToText = ': '.$termPointVal['qty_from'].' - '. ($termPointVal['qty_to'] > 999) ? 'Above':$termPointVal['qty_to']))
+                                                                <?php 
+                                                                $qtyToTxt = ($termPointVal['qty_to'] > 999) ? 'Above':$termPointVal['qty_to'];
+                                                                ?>
+                                                                @php($fromToText = ': '.$termPointVal['qty_from'].' - '.$qtyToTxt)
                                                                 @if($termPointVal['qty_from']-$termPointVal['qty_to'] == 0)
                                                                 @php($fromToText = '')
                                                                 @endif

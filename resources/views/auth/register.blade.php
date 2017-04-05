41d69fb4b2ae364b8af92c3cce84185a8d800e8c
umair321 <P@werfull1>
2017-04-05 20:39:19 +0500

    PP

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,161 +1,137 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="loginc">
-        <div class="text-center col-md-10 col-md-offset-1">
-            <h3 class="slh">SIGN UP FORM</h3>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-heading">
+                    Get registered for free
+                </h1>
+                <hr class="light full">
+                <div class="page-content">
+                    <div class="form-container">
+                        <!--Form-->
+                        <div class="col-md-12">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                                {{ csrf_field() }}
 
-            <hr class="signupline">
-        </div>
-    </section>
-    <!-- .....................Login Form Start............................... -->
-    <section>
-        <div class="container">
-            <div class="row smain">
-
-                <div class="main-login main-center">
-                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}"></div>
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group" id="sformbox">
+                                <div class="form-group" id="sformbox">
 
 
-                            <div class="cols-md-12" id="withbox">
-                                <div class="input-group">
-									<span id="fbg" class="input-group-addon">
-									<i id="lgfico" class="fa fa-user fa-lg" aria-hidden="true"></i>
-									</span>
-                                    <input id="fullname" type="text" class="form-control" name="name" placeholder="Name"
-                                           required autofocus>
+                                    <div class="cols-md-12" id="withbox">
+                                        <div class="input-group">
+                                            <span id="fbg" class="input-group-addon">
+                                                <i id="lgfico" class="fa fa-user fa-lg" aria-hidden="true"></i>
+                                            </span>
+                                            <input id="fullname" type="text" class="form-control" name="name" placeholder="Name"
+                                                   required autofocus>
+                                        </div>
+
+                                        @if ($errors->has('name'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                        @endif
+
+                                    </div>
                                 </div>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span>
-                                @endif
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}" id="sformbox">
 
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}" id="sformbox">
-
-                            <div class="cols-md-12" id="withbox">
-                                <div class="input-group">
-                                    <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-envelope fa-lg"
-                                                                                aria-hidden="true"></i></span>
-                                    <input id="email" type="email" class="form-control" name="email" placeholder="Email"
-                                           required>
+                                    <div class="cols-md-12" id="withbox">
+                                        <div class="input-group">
+                                            <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-envelope fa-lg"
+                                                                                        aria-hidden="true"></i></span>
+                                            <input id="email" type="email" class="form-control" name="email" placeholder="Email"
+                                                   required>
 
 
+                                        </div>
+
+                                        @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                        @endif
+
+                                    </div>
                                 </div>
-
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                <span class="help-block">
                                     <strong>{{ $errors->first('email') }}</strong>
                                 </span>
                                 @endif
 
-                            </div>
-                        </div>
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                        @endif
+
+                                <div class="form-group">
 
 
-                        <div class="form-group">
+                                    <div class="cols-md-10" id="withbox">
+                                        <div class="input-group">
+                                            <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-key fa-lg"
+                                                                                        aria-hidden="true"></i></span>
+                                            <input id="password" type="password" class="form-control" placeholder="Password"
+                                                   name="password" required>
+                                        </div>
 
+                                        @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
 
-                            <div class="cols-md-10" id="withbox">
-                                <div class="input-group">
-                                    <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-key fa-lg"
-                                                                                aria-hidden="true"></i></span>
-                                    <input id="password" type="password" class="form-control" placeholder="Password"
-                                           name="password" required>
+                                    </div>
                                 </div>
-
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
                                 </span>
                                 @endif
+                                <div class="form-group" id="sformbox">
 
-                            </div>
-                        </div>
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                        @endif
-                        <div class="form-group" id="sformbox">
-
-                            <div class="cols-md-12" id="withbox">
-                                <div class="input-group">
-                                    <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-key fa-lg"
-                                                                                aria-hidden="true"></i></span>
-                                    <input id="retypepassword" type="password" class="form-control"
-                                           placeholder="Retype Password" name="password_confirmation" required>
+                                    <div class="cols-md-12" id="withbox">
+                                        <div class="input-group">
+                                            <span id="fbg" class="input-group-addon"><i id="lgfico" class="fa fa-key fa-lg"
+                                                                                        aria-hidden="true"></i></span>
+                                            <input id="retypepassword" type="password" class="form-control"
+                                                   placeholder="Retype Password" name="password_confirmation" required>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+
+
+                                <div class="col-md-6 no-padding">
+                                    <div class="form-group " id="withbox">
+                                        <label class="form-check-label">
+                                            <input required type="checkbox" name="terms" class="form-check-input"/>
+                                            <span id="remberbox"> I agree with <a target="_blank" href="{{route('privacyPolicy')}}" style="color:#92b712">Terms and conditions.</a> </span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 no-padding text-right">
+                                    <div class="form-group " id="withbox">
+                                    <a class="signuplink" href="/login">
+                                        Login to your account
+                                    </a>
+                                    </div>
+                                </div>
+                              
+                                <div class="form-group " id="withbox">
+                                    <button type="submit" class="btn btn-block btn-lg btn-green">Sign Up</button>
+                                </div>
+
+
+
+
+                            </form>
                         </div>
-                        {{--asdasd--}}
-
-                        <div class="form-group " id="withbox">
-                            <label class="form-check-label">
-
-                                <input required type="checkbox" name="terms" class="form-check-input"/>
-                                <span id="remberbox"> I agree with <a href="#" style="color:#92b712">Terms and conditions.</a> </span>
-                            </label>
-                        </div>
-                        <div class="form-group " id="withbox">
-                            <a class="signuplink" href="{{ route('signUp') }}">Login to your account</a>
-                            <button type="submit" class="btn  btn-lg btn-block login-button">Sign Up</button>
-                        </div>
-
-
-                        {{--asdasda--}}
-                        {{--<div class="row">--}}
-                        {{--<div class="col-lg-1">--}}
-                        {{--<div class="input-group">--}}
-                        {{--<input type="checkbox" aria-label="Checkbox for following text input"/>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-lg-5">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<label>I Agree</label>--}}
-                        {{--</div>--}}
-
-                        {{--</div>--}}
-                        {{--<div class="col-lg-5">--}}
-                        {{--<div class="form-group">--}}
-                        {{--<button type="submit" class="btn btn-success">REGISTER</button>--}}
-                        {{--</div>--}}
-
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group">--}}
-                        {{--<input id="terms" type="checkbox" name="terms" required class="form-check-input">--}}
-                        {{--<label>--}}
-                        {{--I agree--}}
-
-                        {{--</label>--}}
-
-                        {{--<a href="" id="termlink"> Terms of Service Privacy Policy</a>--}}
-                        {{--</div>--}}
-
-
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- .....................Login Form End............................... -->
-
-
-    </section>
+    </div>
+</section>
 
 
 

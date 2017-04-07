9a67d97f495ab05fbbecd27de0fd7e02e791b96a
umair321 <P@werfull1>
2017-04-07 19:23:17 +0500

    ashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,6 @@
         {!! Html::style('assets-new/css/creative.css') !!}
         {!! Html::style('assets-new/css/style.css') !!}
         {!! Html::style('assets-new/vendor/basic-table/basictable.css') !!}
-        @yield('css')
     </head>
     <body>
         <nav class="bg-primary navbar navbar-default navbar-fixed-top">
@@ -105,11 +104,11 @@
                                     Start playing
                                 </a>
                             </li>
-                            <li>
+<!--                            <li>
                                 <a href="{{}}" class="page-scroll">
                                     My Team
                                 </a>
-                            </li>
+                            </li>-->
                             <!--                            <li>
                                                             <a class="page-scroll" href="{{route('UserDashboard')}}">DashBoard</a>
                                                         </li>-->

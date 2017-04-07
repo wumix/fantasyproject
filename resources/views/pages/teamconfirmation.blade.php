15a44a11c9899b98e12e4d5739049a33e31e6e6b
umair321 <P@werfull1>
2017-04-07 19:08:44 +0500

    ashboard

--- resources/views/pages/teamconfirmation.blade.php
+++ resources/views/pages/teamconfirmation.blade.php
@@ -20,7 +20,7 @@
                     </p>
                     <p>
                         We will make you updated from your team performance by emails. 
-                        You can also see your team score go to your <a href="{{UserDashboard}}">dashboard</a>.
+                        You can also see your team score go to your <a href="{{route('UserDashboard')}}">dashboard</a>.
                     </p>
                 </div>
             </div>

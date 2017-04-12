9a5b246e6e65f8a2f211900a7aa5995bc63001b4
umair321 <P@werfull1>
2017-04-12 10:46:44 +0500

    Popup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-        {!! Html::style('http://localhost/gamithon_html/vendor/magnific-popup/magnific-popup.css') !!}
+        {!! Html::style('assets-new/vendor/magnific-popup/magnific-popup.css') !!}
         {!! Html::style('assets/css/slicknav.css') !!}
         {!! Html::style('assets-new/css/creative.css') !!}
         {!! Html::style('assets-new/css/style.css') !!}

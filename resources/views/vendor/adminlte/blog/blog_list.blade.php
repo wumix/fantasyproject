406cebac8fa74a5069868488e5c42057f07c9e7b
umair321 <P@werfull1>
2017-04-14 20:21:37 +0500

    post add/edit

--- resources/views/vendor/adminlte/blog/blog_list.blade.php
+++ resources/views/vendor/adminlte/blog/blog_list.blade.php
@@ -1,45 +1,58 @@
 @extends('adminlte::layouts.app')
 
 @section('htmlheader_title')
-    Blog list
+Blog list
 @endsection
 
 @section('main-content')
-    <div class="container-fluid spark-screen">
-        <div class="row">
-            <div class="col-md-12">
-                <!-- Default box -->
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">
-                            Blogs
-                        </h3>
-                    </div>
-                    <div class="box-body">
-                        @if(count($posts) > 0)
-                            <div class="table-responsive">
-
-                                @foreach($posts as $blogposts)
-                                    <div class="jumbotron">
-                                        <h1>{{$blogposts['title']}}</h1>
-                                        <p>{{$blogposts['content']}}</p>
-                                        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-                                    </div>
-
-                                @endforeach
-
-                            </div>
-                        @else
-                            <div class="alert alert-info">
-                                No blog found.
-                            </div>
-                        @endif
-
-                    </div>
-                    <!-- /.box-body -->
+<div class="container-fluid spark-screen">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Default box -->
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">
+                        Blogs
+                    </h3>
                 </div>
-                <!-- /.box -->
+                <div class="box-body">
+                    @if(count($posts) > 0)
+                    <div class="table-responsive">
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <th>Title</th>
+                                <th>Action</th>
+                            </tr>
+                            @foreach($posts as $post)
+                            <tr>
+                                <td>
+                                    <a href="">
+                                        {{$post['title']}}
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{route('editPost', ['blog_id'=>$post['id']])}}" class="btn btn-warning">
+                                        Edit
+                                    </a>
+                                    <a href="" class="btn btn-danger" onclick="return confirm('Sure you wana delete this ***!')">
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                    @else
+                    <div class="alert alert-info">
+                        No blog found.
+                    </div>
+                    @endif
+
+                </div>
+                <!-- /.box-body -->
             </div>
+            <!-- /.box -->
         </div>
     </div>
+</div>
 @endsection

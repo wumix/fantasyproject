7f30305ed1b92dbccb5c6fac1de9153bdc9c5479
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-20 14:55:19 +0500

    merge

---
+++ resources/views/mail/leaderboard.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+</head>
+
+Dear {{$name}}, <br>
+You are among top three in Gamithon LeaderBoard. Follow Up and win exciting prizes at <a href="http://www.gamithonfantasy.com"> gamithonfanstasy</a>
+
+
+</body>
+</html>

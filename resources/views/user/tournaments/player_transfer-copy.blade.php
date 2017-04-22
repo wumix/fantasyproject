8d5cf48f5fa1c2304d818a111686ade68a8df9cd
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-22 14:42:59 +0500

    Merge

---
+++ resources/views/user/tournaments/player_transfer-copy.blade.php
@@ -0,0 +1,250 @@
+@php
+
+
+//dd($roles);
+//dd($player_info);
+
+        @endphp
+@extends('layouts.app')
+
+@section('content')
+
+
+    <section class="loginc">
+        <div class="text-center col-md-10 col-md-offset-1">
+
+            <h3 class="slh">{{$team_name}}</h3>
+            <hr class="signupline">
+
+            <input id="team_id" type="hidden" value="{{$team_id}}">
+            <h3 class="tnt">{{$tournament_detail['name']}}</h3>
+        </div>
+    </section>
+    <!-- .....................TeamStart............................... -->
+    {{--<section class="myteam">--}}
+        {{--<div class="countaner">--}}
+            {{--<div class="col-md-3"></div>--}}
+            {{--<section>--}}
+                {{--<div id="mytl" class="text-center col-md-3">--}}
+                    {{--<h3 class="tnt">PLAYER ROLE LIMIT</h3>--}}
+                    {{--<h5 class="myt">You can select players within this limit</h5>--}}
+                    {{--<br>--}}
+                    {{--<h4 class="myt1">Batsmen: 4</h4>--}}
+                    {{--<h4 class="myt1">Bowler: 4</h4>--}}
+                    {{--<h4 class="myt1">Allrounders: 2</h4>--}}
+                    {{--<h4 class="myt1">Wicketkeeper: 1</h4>--}}
+
+                {{--</div>--}}
+            {{--</section>--}}
+
+            {{--<section>--}}
+                {{--<div id="myt2" class="text-center col-md-3">--}}
+                    {{--<h3 class="tnt1">PLAYER ROLE LIMIT</h3>--}}
+                    {{--<h5 class="myt">Selected team players</h5>--}}
+                    {{--<br>--}}
+                    {{--<h4 class="myt2">Batsmen: <span>4</span></h4>--}}
+                    {{--<h4 class="myt2">Bowler: <span>4</span></h4>--}}
+                    {{--<h4 class="myt2">Allrounders: <span>2</span></h4>--}}
+                    {{--<h4 class="myt2">Wicketkeeper: <span>1</span></h4>--}}
+
+                {{--</div>--}}
+
+            {{--</section>--}}
+        {{--</div>--}}
+    {{--</section>--}}
+    <!-- .....................Team End............................... -->
+    <!-- ..............................Table Start................................... -->
+    <section>
+        <div class="container-fluid" id="tblmarconten">
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <div class="text-center">
+                            <h3 class="ah3">Player Transfer</h3>
+                        </div>
+                    </div>
+
+
+                </div>
+                <br>
+                <br>
+                <br>
+                <br>
+                <table class="table table-striped" id="tortable">
+                    <thead class="main-taible-head">
+                    <tr>
+                        <th class="border-r th1">PLAYERS</th>
+                        <th class="border-r">ROLE</th>
+                        <th class="border-r">POINTS</th>
+
+                    </tr>
+                    </thead>
+                    <tbody id="selected-player" class="main-taible-body">
+
+                        <tr>
+                            <td class="border-r1">
+                               {{$player_info['name']}}
+                            </td>
+
+                            <td class="border-r1"><p class="myteamtt">{{$player_info['player_roles'][0]['name']}}</p></td>
+                            <td class="border-r1"><p class="myteamtt">{{$player_info['player_tournaments'][0]['pivot']['player_price']}}</p></td>
+                            <td>
+
+                                    </td>
+
+                        </tr>
+
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+
+    <!-- ...............Table End.............................. -->
+    <div class="text-center">
+        <h3 class="ah3">Players Available For Transfer</h3>
+    </div>
+
+    <section>
+
+        <div class="container">
+
+
+            <div class="panel with-nav-tabs panel">
+                <div class="panel-heading">
+                    <ul class="nav nav-tabs">
+
+                        @foreach($roles as $key => $role)
+
+                            <li id="tbbox-{{$key}}" class="{!! ($key == 0) ? 'active':'' !!}">
+                                <a id="tabt-{{$key}}" href="#tab{{$role['id']}}default"
+                                   data-toggle="tab">{{$role['name']}}</a>
+                            </li>
+
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        {{--first div--}}
+                        @foreach($roles as $key=>$role)
+                            <div class="tab-pane fade {!! ($key == 0) ? 'in active':'' !!}"
+                                 id="tab{{$role['id']}}default">
+                                <div class="col-md-11 tcen ">
+                                    <table class="table " id="tortable">
+                                        <thead class="main-taible-head1">
+                                        <tr>
+
+                                            <th class=" th1">PLAYERS</th>
+
+                                            <th class="text-center">PRICE</th>
+                                            <th class="text-center">ACTION</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="main-taible-body">
+
+                                        @foreach($role['players'] as $player)
+                                            <tr id="player_tr-{{$player['id']}}" class="cwt">
+
+                                                <td class=" th11"><img id="myteamtimg" class="img-circle"
+                                                  style="width: 80px;float: left;margin-right: 24px;" src="{{getUploadsPath($player['profile_pic'])}}"> {{$player['name']}}
+                                                </td>
+
+                                                <td class=" point"><p
+                                                            class="myteamtt">
+                                                        <?php
+                                                        $playerids= 0;
+                                                        $playerThisTournamnetPrice = 0;
+                                                       // dd($player);
+                                                        if (!empty($player['player_tournaments'][0]['pivot']['player_price'])) {
+                                                        $playerThisTournamnetPrice = $player['player_tournaments'][0]['pivot']['player_price'];
+                                                        }
+
+//                                                            if (!empty($player['player_tournaments'][0]['id'])) {
+//                                                                $playerids =$player['player_tournaments'][0]['id'];
+//                                                            }
+                                                            ?>
+                                                           {{$playerThisTournamnetPrice}}
+
+                                                    </p>
+                                                </td>
+
+                                                <td>
+                                                    <a onclick="return confirm('Are you sure you want to transfer');" style="text-transform: uppercase"  id="btn-player-{{$player['id']}}"href="javascript:addplayertoteam('{{$role['name']}}','{{$role['id']}}','{{$player['id']}}','1','{{$playerThisTournamnetPrice}}')" class="btn btn-md bttor1">
+                                                        TRANSFER WITH {{$player_info['name']}}
+                                                    </a>
+                                                </td>
+
+
+                                            </tr>
+                                        @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        @endforeach
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--/Choose player-->
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+@endsection
+@section('addteamjs')
+
+    <script>
+        function addplayertoteam(rolename, roleid, playerin, tournamentid, player_price) {
+
+            var arr_player_id = [];
+            arr_player_id.push(playerin);
+
+
+
+            $.ajax({
+                type: 'POST',
+                url: '{{route('transferPlayerAjax')}}',
+                data: {
+                    tournament_id: tournamentid,
+                    player_in_id: arr_player_id[0],
+                    player_out_id:{{$player_info['id']}},
+                    role_id: roleid,
+                    role_name: rolename,
+                    player_out_price:{{$player_info['player_tournaments'][0]['pivot']['player_price']}},
+                    team_id:{{$team_id}}, _token: '{{csrf_token()}}'
+                },
+                success: function (data) {
+                    if (data.success == true) {
+                        $('.error').html(data.msg);
+                        $('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+                        var url = '{{route("teamdetail", ['team_id'=>'ttid']) }}';
+
+                        url = url.replace('ttid', data.team_id);
+                     //   url = url.replace('tid', data.tournament_id);
+                       //alert(url);
+                  window.location.href = url;
+
+                    } else {
+                        $('.error').html(data.msg);
+                        $('.error').fadeIn(400).delay(7000).fadeOut(400); //fade out after 3 seconds
+
+
+                    }
+
+                }
+            });
+        }
+    </script>
+
+@endsection

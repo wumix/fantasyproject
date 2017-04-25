5e069f6ee6272c1555a09c6392e17dba8b97d838
Ahmad Mughal <Ahmad Mughal>
2017-04-25 16:26:07 +0500

    merge

--- resources/views/user/blog/blog.blade.php
+++ resources/views/user/blog/blog.blade.php
@@ -6,8 +6,8 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="text-center">
-                    Blog
+                <h1 class="text-center" style="color: #F9960E;">
+                    BLOG
                 </h1>
                 <hr class="light full">
 
@@ -31,8 +31,8 @@
                             <a href="{{route('showBlogPostDetail',['post_id'=>$row['id']])}}" class="bloghead" >{{$row['title']}}</a>
                             <p class="blogtext">{!! $row['description'] !!} 
                         </div>
-                        
-                            <span>Lorem and his option </span>
+
+                            <span style="padding-left: 10px;">Lorem and his option </span>
                     </div>
                 </div>
                 @endforeach

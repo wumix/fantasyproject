edd1ccf56c09f8f0f9772a47bdc6f9156038244b
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-27 11:16:35 +0500

    asd

--- resources/views/user/blog/blog-inner.blade.php
+++ resources/views/user/blog/blog-inner.blade.php
@@ -34,18 +34,18 @@
                                 By Admin
                             </p>
 
-                            {{--<p class="text-right bloginnertext chattextico col-md-2">--}}
-                            {{--<div id="fb-root"></div>--}}
-                            {{--<script>(function(d, s, id) {--}}
-                                    {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
-                                    {{--if (d.getElementById(id)) return;--}}
-                                    {{--js = d.createElement(s); js.id = id;--}}
-                                    {{--js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1736071000056030";--}}
-                                    {{--fjs.parentNode.insertBefore(js, fjs);--}}
-                                {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
-                            {{--<div class="fb-share-button" data-href="http://www.gamithonfantasy.com" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gamithonfantasy.com%2F&amp;src=sdkpreparse">Share</a></div>--}}
+                            <p class="text-right bloginnertext chattextico col-md-2">
+                            <div id="fb-root"></div>
+                            <script>(function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) return;
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1736071000056030";
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));</script>
+                            <div class="fb-share-button" data-href="http://www.gamithonfantasy.com" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gamithonfantasy.com%2F&amp;src=sdkpreparse">Share</a></div>
 
-                            {{--</p>--}}
+                            </p>
 
                         </div>
                         <hr class="bloghr">

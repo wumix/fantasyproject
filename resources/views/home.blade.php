fdf1852c9b3cdcbc74da50314135ac7e208886ff
Ahmad Mughal <Ahmad Mughal>
2017-04-27 18:50:40 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -108,15 +108,16 @@
     <!-----------    ----------->
     <section class="bg-primary no-padding" id="about">
         <div class="container-fluid no-padding">
-            <div class="col-lg-2 text-center no-padding" style="background: orange; font-weight: bold;">
+            <div class="col-md-12">
+            <div class="col-lg-2 text-center no-padding" style="background: orange; font-weight: bold; min-height: 30px;">
                 <div class="table-responsive" style="padding: 5px;">
                 Latest News
                 </div>
             </div>
-            <div class="col-lg-10 text-center no-padding">
+            <div class="col-lg-10 text-center no-padding" style="min-height: 30px;">
 
 
-                <div class="table-responsive no-padding" style="background: #92B713;">
+                <div class="table-responsive no-padding" style="background: #92B713; min-height: 30px;">
                     <marquee style="padding: 5px; padding-bottom: 0; font-weight: bold;">
                         <?php $ch = curl_init('http://www.espncricinfo.com/rss/content/story/feeds/1078425.xml');
                         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -141,6 +142,7 @@
                     </marquee>
                 </div>
 
+            </div>
             </div>
         </div>
     </section>

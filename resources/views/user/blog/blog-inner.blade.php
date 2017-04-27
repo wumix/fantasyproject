b2830d55bcfcb7db2b4c82c6a625c4046f4476b7
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-27 12:33:18 +0500

    asd

--- resources/views/user/blog/blog-inner.blade.php
+++ resources/views/user/blog/blog-inner.blade.php
@@ -48,7 +48,7 @@
                                     js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1736071000056030";
                                     fjs.parentNode.insertBefore(js, fjs);
                                 }(document, 'script', 'facebook-jssdk'));</script>
-                            <div class="fb-share-button" data-href="http://www.gamithonfantasy.com" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gamithonfantasy.com%2F&amp;src=sdkpreparse">Share</a></div>
+                            {{--<div class="fb-share-button" data-href="http://www.gamithonfantasy.com" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gamithonfantasy.com%2F&amp;src=sdkpreparse">Share</a></div>--}}
 
                             </p>
 

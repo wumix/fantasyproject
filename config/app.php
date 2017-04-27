da1b0aaa27e5767c729b881cc7f5fc3e678a7aa7
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-27 22:04:53 +0500

    merge

--- config/app.php
+++ config/app.php
@@ -142,7 +142,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Laravelista\Comments\Providers\CommentsServiceProvider::class,
+
 
         /*
          * Package Service Providers...

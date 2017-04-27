020a36c1e9b546fbf85d587f90a553a6138c4d1d
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-04-27 11:42:12 +0500

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
-    <meta property="og:url"                content="http://gamithonfantasy.com/blog/18" />
+    <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
     <meta property="og:type"               content="article" />
-    <meta property="og:title"              content="Gamithion Blog Post" />
-    <meta property="og:description"        content="Gamithon Fantasy />
-    <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
-    <link rel="icon" href="../img/gamithon-fevi.ico" type="image/x-icon"/>
+    <meta property="og:title"              content="Welcome to gamithon" />
+    <meta property="og:description"        content="This is gamithon Fantasy" />
+    <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" /> <link rel="icon" href="../img/gamithon-fevi.ico" type="image/x-icon"/>
     <link rel="shortcut icon" href="../img/gamithon-fevi.ico" type="image/x-icon"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">

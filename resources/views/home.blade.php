690ae45da7dac802df5f69e491a630b50bd9851e
Ahmad Mughal <Ahmad Mughal>
2017-05-04 17:50:01 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -106,46 +106,7 @@
     <!--BASBB-->
     <!-- ..........................Banner Start ............................-->
     <!-----------    ----------->
-    <section class="bg-primary no-padding" id="about">
-        <div class="container-fluid no-padding">
-            <div class="col-md-12 no-padding">
-            <div class="col-lg-2 text-center no-padding" style="background: orange; font-weight: bold; height: 30px;">
-                <div class="table-responsive" style="padding: 5px;">
-                    Latest News
-                </div>
-            </div>
-            <div class="col-lg-10 text-center no-padding" style="height: 30px;">
-
-
-                <div class="table-responsive no-padding" style="background: #92B713; height: 30px;">
-                    <marquee style="padding: 5px; padding-bottom: 0; font-weight: bold;">
-                        <?php $ch = curl_init('http://www.espncricinfo.com/rss/content/story/feeds/1078425.xml');
-                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                        $result = curl_exec($ch);
-                        //$array = json_decode($result, true);
-                        $doc = new SimpleXmlElement($result, LIBXML_NOCDATA);
-
-                        $doc = $doc->channel;
-
-
-                        $doc = $doc->item;
-
-
-                        foreach ($doc as $row) {
-
-                            echo ' '.$row->description.'.';
-                            echo'&nbsp'; echo'&nbsp'; echo'&nbsp';
-
-                        }
-                        ?>
-
-                    </marquee>
-                </div>
-
-            </div>
-            </div>
-        </div>
-    </section>
+    
 
 
     <section class="bg-primary" id="about">

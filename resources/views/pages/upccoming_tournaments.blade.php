d808b137a07c5488d85aee7a91de19a90af1b40b
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-10 13:14:46 +0500

    merge

---
+++ resources/views/pages/upccoming_tournaments.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="text-center" style="color: #F9960E;">
+                    Upcomming Tournaments
+                </h1>
+                <hr class="light full">
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="publicaciones-blog-home no-padding">
+    <div class="container">
+        <div class="row" id="adm">
+            <div id="mywebinar" class="row">
+                <div class="col-sm-10">
+                    <h4><b>LAMB OF GOOD TOUR <sub>1</sub></b></h4></div>
+
+                <div class="col-sm-1">
+                    <img style="padding:15px 0 10px 150px;" src="images1\a\images\Ongoing-Webinar_Presenter_Admin_My-Webinar_03.png"></div>
+            </div>
+            <div class="row">
+
+
+                <div class="col-sm-2" style="background-color: white;">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <img src="images1\a\images\Ongoing-Webinar_Presenter_Admin_My-Webinar_10.png"></div>
+                        <div class="col-sm-1">
+                            <p style="color: #C0C0C0;"> Date:<b style="color: black;">03/3/2017</b></p></div>
+                    </div>
+
+                </div>
+                <div class="col-sm-2" style="background-color: white;">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <img src="images1\a\images\Ongoing-Webinar_Presenter_Admin_My-Webinar_12.png"></div>
+                        <div class="col-sm-1">
+                            <p style="color: #C0C0C0;"> Time:<b style="color: black;">o6:ooP.M</b></p></div>
+                    </div>
+
+                </div>
+                <div class="col-sm-7" style="background-color: white;">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <img src="images1\a\images\Ongoing-Webinar_Presenter_Admin_My-Webinar_07.png">
+                        </div>
+                        <div class="col-sm-7">
+                            <p style="color: #C0C0C0;"> Speakers:<b style="color: black;">Carmen Okabe, Hassan Ahmad Chaudhry</b></p></div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-5" style="background-color: white;">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <img src="images1\a\images\Ongoing-Webinar_Presenter_Admin_My-Webinar_18.png"></div>
+                        <div class="col-sm-10">
+                            <p style="color: #C0C0C0;"> Fee:<b style="color: black;">PKR 25oo/ - (Normal)</b>&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;<b style="color: black;">PKR 35oo/ - (Hot Seat)</b></p></div>
+
+                    </div>
+                    <div class="row" style="padding-bottom: 20px;">
+                        <button type="button" class="btn btn-success btn-sm round">Create Webinar</button>
+                        <button type="button" class="btn btn-success btn-sm round">Create Webinar</button>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+</section>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+@endsection

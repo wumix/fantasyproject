1d602bbf21011e1943f240406f4dea3ef1053659
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-12 21:41:44 +0500

    merge

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+
+public function showNewsDetail($id){
+    $data['postdetail'] = \App\BlogPost::where('id',$id)->firstOrFail()->toArray();
+
+    return view('user.news.newsinner', $data);
+
+}
+
+
+
+
+}

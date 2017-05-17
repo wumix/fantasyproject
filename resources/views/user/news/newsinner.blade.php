2410421eacaf0f0a3ef13707559790eb96bafa8a
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-17 18:53:24 +0500

    merge

--- resources/views/user/news/newsinner.blade.php
+++ resources/views/user/news/newsinner.blade.php
@@ -22,7 +22,7 @@
             if (d.getElementById(id)) return;
             js = d.createElement(s);
             js.id = id;
-            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1736071000056030";
+            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=712839212231397";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
 @stop

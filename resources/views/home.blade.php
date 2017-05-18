aa8974f90f8c9ff751c6de81c59f86a50ad6f3df
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-18 15:44:02 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -509,7 +509,7 @@
         $('#header').backstretch([
 
 
-            {url: '{{Url::to('assets-new/img/gamithon-final-2.jpg')}}', fade: 1000},
+            {url: '{{Url::to('assets-new/img/gamithon-actual-cover-size.jpg')}}', fade: 1000},
             {url: '{{Url::to('assets-new/img/yp-1.jpg')}}', fade: 1000},
             {url: '{{Url::to('assets-new/img/bg1.jpg')}}', fade: 1000},
 

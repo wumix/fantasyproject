f7255b7bb4f98325e49940c05d20c51127b11f17
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-26 15:49:40 +0500

    "NEWS INNER PAGE"

--- resources/views/user/team_detail.blade.php
+++ resources/views/user/team_detail.blade.php
@@ -223,11 +223,12 @@ foreach ($team_score as $teamplayers) {
 
 @section('js')
     <script>
-        $(document).ready(function () {
+
+
             $('[data-toggle="tooltip"]').tooltip();
             $('#team_score').html('{{$teamtotal}}')
             $('#remaining_score').html('{{getUserTotalScore(\Auth::id())}}');
-        });
+
     </script>
 @stop
 

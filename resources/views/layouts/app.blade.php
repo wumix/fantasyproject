ed1dd34d835e9b4272b2c3309e4188edb4322d11
abdul786 <abdul.rehman@branchezconsulting.com>
2017-05-31 11:18:41 -0700

    merge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
     <!-- Bootstrap Core CSS -->
 {!! Html::style('assets-new/vendor/bootstrap/css/bootstrap.min.css') !!}
 {!! Html::style('assets-new/css/bootstrap-tour.css') !!}
-{!! Html::style('assets-new/css/jquery.mCustomScrollbar.css') !!}
 {!! Html::style('https://fonts.googleapis.com/css?family=Raleway') !!}
 {!! Html::style('assets-new/vendor/font-awesome/css/font-awesome.min.css') !!}
 {!! Html::style('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
@@ -47,10 +46,14 @@
             margin: 0 0 0 0;
 
         }
+
+
+
+
     </style>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
@@ -282,14 +285,13 @@
 {!! Html::script('assets-new/js/jquery.mCustomScrollbar.concat.min.js') !!}
 
 
+
 <script>
   
 
     $(document).ready(function () {
         $('.itemsz').matchHeight();
-     $(window).on('load', function(){
-            $('.content').mCustomScrollbar();
-        });
+//    $('.content').mCustomScrollbar();
         $('#bs-example-navbar-collapse-1').slicknav({
             appendTo: '#top-menu-res',
             label: 'Main Menu'
@@ -300,10 +302,7 @@
             opacity: 0.3,
             speed:'700'
         });
-            $('.content').mCustomScrollbar({
-        theme:"light-thin",
-                axis:"x"
-    });
+
     });
 </script>
 {!! Html::script('assets-new/js/jquery.matchHeight-min.js') !!}

18a69454a43939f49dd60f04ee35511e14499ef8
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-05-31 18:01:33 +0500

    merge

--- resources/views/user/dashboard/dashboard.blade.php
+++ resources/views/user/dashboard/dashboard.blade.php
@@ -516,18 +516,18 @@
                         </div>
                         <div class="col-md-6">
                             <ul class="medal">
-                                <li id="exp">
-                                    <img src={{URL::to('/img/gold-medal.png')}} alt=""/>
+                                {{--<li id="exp">--}}
+                                    {{--<img src={{URL::to('/img/gold-medal.png')}} alt=""/>--}}
                                     {{--<span>20</span>--}}
-                                </li>
+                                {{--</li>--}}
                                 {{--<li id="exp1">--}}
                                     {{--<img src={{URL::to('/img/gold-medal1.png')}} alt=""/>--}}
                                     {{--<span>10</span>--}}
                                 {{--</li>--}}
-                                {{--<li id="exp2">--}}
-                                    {{--<img src={{URL::to('/img/gold-medal2.png')}} alt=""/>--}}
-                                    {{--<span>00</span>--}}
-                                {{--</li>--}}
+                                <li id="exp2">
+                                    <img src={{URL::to('/img/gold-medal2.png')}} alt=""/>
+                                    <span>00</span>
+                                </li>
                             </ul>
 
                         </div>

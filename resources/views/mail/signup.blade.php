6ac1528d07fe0f922b72edcee10f1bf013d8bd5b
umair321 <P@werfull1>
2017-06-01 17:14:52 +0500

    Signup email

--- resources/views/mail/signup.blade.php
+++ resources/views/mail/signup.blade.php
@@ -3,7 +3,7 @@
 {!! Html::style('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
 {!! Html::style('https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic') !!}
 
-<style>
+<style type="text/css">
     .whole_setio_news {
         width: 800px;
         display: block;

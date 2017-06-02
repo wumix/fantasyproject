561382302f692550ed194a7835649c23b14f4b86
umair321 <P@werfull1>
2017-06-02 16:10:20 +0500

    reset pass

--- resources/views/vendor/adminlte/auth/passwords/email.blade.php
+++ resources/views/vendor/adminlte/auth/passwords/email.blade.php
@@ -1,75 +1,91 @@
-@extends('adminlte::layouts.auth')
-
-@section('htmlheader_title')
+@extends('layouts.app')
+@section('title')
     Password recovery
-@endsection
+@stop
+@section('css')
+    <style>
+        section.content {
+            min-height: 100%;
+        }
 
+        .page-content {
+            position: absolute;
+            transform: translate(-50%, 50%);
+            top: 50%;
+            left: 50%;
+            max-width: 700px;
+            margin: 0 auto;
+        }
+
+        .container .row {
+            position: relative;
+        }
+
+    </style>
+
+@stop
 @section('content')
+    <section class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-heading">
+                        Reset Password
+                    </h1>
+                    <hr class="light full">
+                    <div class="page-content">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="login-box-body">
+                            <div class="col-md-12 no-padding">
+                                <p class="help help-block">
+                                    Please enter your email, we will send password reset link to you.
+                                </p>
+                            </div>
 
-<body class="login-page">
-    <div id="app">
+                            <form action="{{ url('/password/email') }}" method="post">
 
-        <div class="login-box">
-        <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
-        </div><!-- /.login-logo -->
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <div class="form-group has-feedback">
+                                    <input required type="email" class="form-control" placeholder="Email" name="email"
+                                           value="{{ old('email') }}"/>
+                                </div>
 
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
+                                <div class="form-group">
+                                    <button type="submit"
+                                            class="btn btn-primary btn-block btn-flat">
+                                        {{ trans('adminlte_lang::message.sendpassword') }}
+                                    </button>
+                                </div>
+                            </form>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a href="{{ url('/login') }}">Log in</a>
+                                </div>
+                                <div class="col-md-6 pull-right text-right">
+                                    <a href="{{ route('signUp') }}"
+                                       class="text-center">{{ trans('adminlte_lang::message.registermember') }}
+                                    </a>
+                                </div>
+                            </div>
 
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
 
-        <div class="login-box-body">
-            <p class="login-box-msg">Reset Password</p>
-            <form action="{{ url('/password/email') }}" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group has-feedback">
-                    <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}"/>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                        </div><!-- /.login-box-body -->
+                    </div><!-- /.login-box -->
                 </div>
-
-                <div class="row">
-                    <div class="col-xs-2">
-                    </div><!-- /.col -->
-                    <div class="col-xs-8">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.sendpassword') }}</button>
-                    </div><!-- /.col -->
-                    <div class="col-xs-2">
-                    </div><!-- /.col -->
-                </div>
-            </form>
-
-            <a href="{{ url('/login') }}">Log in</a><br>
-            <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a>
-
-        </div><!-- /.login-box-body -->
-
-    </div><!-- /.login-box -->
-    </div>
-
-    @include('adminlte::layouts.partials.scripts_auth')
-
-    <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%' // optional
-            });
-        });
-    </script>
-</body>
-
+            </div>
+        </div>
+    </section>
 @endsection

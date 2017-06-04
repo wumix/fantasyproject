a0f41790c374f0d32abe0da17076fc54e5ded9b6
umair321 <P@werfull1>
2017-06-04 15:28:45 +0500

    Merge

--- resources/views/user/tournaments/add_user_team.blade.php
+++ resources/views/user/tournaments/add_user_team.blade.php
@@ -4,7 +4,9 @@
 
 @endphp
 @extends('layouts.app')
-
+@section('title')
+    Make team
+    @stop
 @section('content')
     <section>
         <div class="container">
@@ -16,19 +18,24 @@
                     <hr class="light full">
                     <div class="page-content">
                         <div class=" col-md-6">
-                            <img style="width: 200px;" src="{{getUploadsPath($tournament_detail['t_logo'])}}" class="img-thumbnail" alt="Responsive image" />
+                            <img style="width: 200px;" src="{{getUploadsPath($tournament_detail['t_logo'])}}"
+                                 class="img-thumbnail" alt="Responsive image"/>
                         </div>
                         <div class="col-md-6">
                             <h3>{{$tournament_detail['name']}}</h3>
                             <br>
-                            <h4 class="tnt1">Tournament Cost:<span class="tnt2">{{$tournament_detail['tournament_price']}}</span></h4>
+                            <h4 class="tnt1">Tournament Cost:<span
+                                        class="tnt2">{{$tournament_detail['tournament_price']}}</span></h4>
                             <br>
-                            <h4 class="tnt1">Current Points:<span class="tnt2">{{getUserTotalScore(Auth::id())}}</span></h4>
+                            <h4 class="tnt1">Current Points:<span class="tnt2">{{getUserTotalScore(Auth::id())}}</span>
+                            </h4>
                             <div class="col-md-5">
                                 <form id="team_submit">
                                     <input type="text" id="team_name" class="fw form-control"
                                            placeholder="Enter Your Team Name">
-                                    <button id="save_button" type="submit" class="btn btn-green btn-lg btn-block mt26">SAVE</button>
+                                    <button id="save_button" type="submit" class="btn btn-green btn-lg btn-block mt26">
+                                        SAVE
+                                    </button>
                                 </form>
                             </div>
 
@@ -77,18 +84,38 @@
                     if (data.status == "ok") {
                         $("#team_name").attr('disabled', true);
                         $("#save_button").remove();
-                        $('.error').html('team added sucessfully');
-                        $('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+                        //$('.error').html('team added sucessfully');
+                        //$('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+                        $.toast({
+                            heading: '&nbsp;',
+                            text: 'Team added sucessfully!',
+                            icon: 'success',
+                            loader: true, // Change it to false to disable loader
+                            position: 'top-right',
+                            showHideTransition: 'slide',
+                            hideAfter: 10000,
+                            allowToastClose: true,
+                            loaderBg: '#92B713'  // To change the background
+                        });
                         window.location.reload();
                         $('<input type="hidden" id="team_id" value="' + data.team_id + '"/>').insertBefore("#addstatus");
+
                     }
                     else {
-
-                        $('.error').html(data.message);
-                        $('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
-
+                        //$('.error').html(data.message);
+                        //$('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+                        $.toast({
+                            heading: 'Error',
+                            text: data.message,
+                            icon: 'error',
+                            loader: true, // Change it to false to disable loader
+                            position: 'top-right',
+                            showHideTransition: 'slide',
+                            hideAfter: 10000,
+                            allowToastClose: true,
+                            loaderBg: '#92B713'  // To change the background
+                        });
                     }
-
                 }
             });
         });

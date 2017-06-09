351fef05d924b86f8cdf9ceac6ee0dd5fb2a8afe
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-10 01:33:13 +0500

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -97,7 +97,7 @@ class HomeController extends Controller
 
     public function leaderboard()
     {
-        $data['leaders'] = \App\Leaderboard::with('user', 'user_team')->take(22)->orderBy('score', 'DESC')->get()->toArray();
+        $data['leaders'] = \App\Leaderboard::with('user', 'user_team')->take(21)->orderBy('score', 'DESC')->get()->toArray();
         return view('pages.leaderboard', $data);
 
     }

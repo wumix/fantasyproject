658e72c1562a8b23b413a1f0b03ea6bb35c3bfee
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-10 02:41:36 +0500

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -322,6 +322,98 @@
             <div class="row no-gutter popup-gallery">
 
 
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/201.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/201.jpg" class="img-responsive" alt="KXIP VS KKR">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                   Bangladesh vs New zealand
+                                </div>
+                                <div class="project-name">
+                                    Bangladesh won by 5 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/202.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/202.jpg" class="img-responsive" alt="KXIP VS KKR">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Bangladesh vs New zealand
+                                </div>
+                                <div class="project-name">
+                                    Bangladesh won by 5 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/203.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/203.jpg" class="img-responsive" alt="KXIP VS MI">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Bangladesh vs New zealand
+                                </div>
+                                <div class="project-name">
+                                    Bangladesh won by 5 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/301.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/301.jpg" class="img-responsive" alt="KXIP VS MI">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Sri Lanka Vs India
+                                </div>
+                                <div class="project-name">
+                                    Sri Lanka won by 7 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/302.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/302.jpg" class="img-responsive" alt="KXIP VS MI">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Sri Lanka Vs India
+                                </div>
+                                <div class="project-name">
+                                    Sri Lanka won by 7 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/303.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/303.jpg" class="img-responsive" alt=" DD VS GL">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Sri Lanka Vs India
+                                </div>
+                                <div class="project-name">
+                                    Sri Lanka won by 7 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
 
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/thumbnails/101.jpg" class="portfolio-box">
@@ -373,51 +465,7 @@
                         </div>
                     </a>
                 </div>
-                {{--<div class="col-lg-4 col-sm-6">--}}
-                    {{--<a href="img/portfolio/thumbnails/201.jpg" class="portfolio-box">--}}
-                        {{--<img src="img/portfolio/thumbnails/201.jpg" class="img-responsive" alt="KXIP VS KKR">--}}
-                        {{--<div class="portfolio-box-caption">--}}
-                            {{--<div class="portfolio-box-caption-content">--}}
-                                {{--<div class="project-category text-faded">--}}
-                                    {{--England vs New zealand--}}
-                                {{--</div>--}}
-                                {{--<div class="project-name">--}}
-                                    {{--England won by 87 runs--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
-                {{--<div class="col-lg-4 col-sm-6">--}}
-                    {{--<a href="img/portfolio/thumbnails/202.jpg" class="portfolio-box">--}}
-                        {{--<img src="img/portfolio/thumbnails/202.jpg" class="img-responsive" alt="KXIP VS KKR">--}}
-                        {{--<div class="portfolio-box-caption">--}}
-                            {{--<div class="portfolio-box-caption-content">--}}
-                                {{--<div class="project-category text-faded">--}}
-                                    {{--England vs New zealand--}}
-                                {{--</div>--}}
-                                {{--<div class="project-name">--}}
-                                    {{--England won by 87 runs--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
-                {{--<div class="col-lg-4 col-sm-6">--}}
-                    {{--<a href="img/portfolio/thumbnails/203.jpg" class="portfolio-box">--}}
-                        {{--<img src="img/portfolio/thumbnails/203.jpg" class="img-responsive" alt="KXIP VS MI">--}}
-                        {{--<div class="portfolio-box-caption">--}}
-                            {{--<div class="portfolio-box-caption-content">--}}
-                                {{--<div class="project-category text-faded">--}}
-                                    {{--England vs New zealand--}}
-                                {{--</div>--}}
-                                {{--<div class="project-name">--}}
-                                    {{--England won by 87 runs--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
+
 
 
 

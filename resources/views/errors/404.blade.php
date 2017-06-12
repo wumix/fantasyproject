944850243ed4210675028ce963b97fc0d2aa2b73
umair321 <P@werfull1>
2017-06-12 17:32:56 +0500

    Dashboard erro

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,10 +4,17 @@
     Page not found
 @stop
 @section('content')
+    <style>
+        footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="page-heading text-center">
+                <h1 class="page-heading text-center" style="font-size: 300px;">
                     404
                 </h1>
             </div>

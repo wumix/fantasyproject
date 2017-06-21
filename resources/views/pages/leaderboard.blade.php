96dbb7378ea209b87aafa5d4ab412bd94e016c20
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-21 18:05:26 +0500

    merge

--- resources/views/pages/leaderboard.blade.php
+++ resources/views/pages/leaderboard.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
-<<<<<<< HEAD
 
-=======
-{{--{{dd($leaders)}}--}}
->>>>>>> develop
 @section('title')
     Leaderboard
 @stop
@@ -333,13 +329,8 @@
             <div class="col-md-12">
                 <h1 class="page-heading">
                     @if(count($leaders)>3)
-<<<<<<< HEAD
-                    Top Players
-                        @else
-=======
                         Top Players
                     @else
->>>>>>> develop
                         Nothing to show
                     @endif
                 </h1>
@@ -347,27 +338,6 @@
             </div>
 
             @if(count($leaders)>3)
-<<<<<<< HEAD
-            <div class="col-md-12">
-                <div class="leader_bord_section">
-
-                    <div class="col-md-6">
-                        <div class="col-md-4 col-sm-12">
-                            <div class="cnter_liez">
-                                <img class="img-responsive "
-                                     src="
-                              @if("http://www.gamithonfantasy.com/assets-new/img/default-profile-pic.png"==getUploadsPath($leaders[0]['user']['profile_pic']))
-                                             {{getUploadsPath($leaders[0]['user']['profile_pic'])}}
-                                                    @else
-                                     <?php
-                                     $check=explode(".",$leaders[0]['user']['profile_pic']);
-                                     if($check[1]=="facebook"){
-                                      echo $leaders[0]['user']['profile_pic'];
-                                     }else{
-                                     echo Croppa::url(getUploadsPath($leaders[0]['user']['profile_pic']),156,134);
-                                     }
-                                         
-=======
                 <div class="col-md-12">
                     <div class="leader_bord_section">
 
@@ -381,147 +351,16 @@
                                          @else
                                          <?php
                                          $check=explode(".",$leaders[0]['user']['profile_pic']);
-                                      //  if($check[1]=="facebook"){
-                                                if(1){
+                                         if($check[1]=="facebook"){
                                              echo $leaders[0]['user']['profile_pic'];
                                          }else{
                                              echo Croppa::url(getUploadsPath($leaders[0]['user']['profile_pic']),156,134);
                                          }
 
->>>>>>> develop
                                          ?>
 
 
 
-<<<<<<< HEAD
-                                             @endif" alt=""/>
-                            </div>
-                            <div class="clear clearfix"></div>
-                        </div>
-                        <div class="col-md-4 text-center area_leader col-sm-12">
-                            <span class="leder_cirlcle_jhon_text">{{$leaders[0]['user']['name']}}</span>
-                            <span class="leder_cirlcle_jhon_text_num">{{$leaders[0]['score']}}</span>
-                            <span class="leder_smaal_text">Current Points</span>
-                        </div>
-                        <div class="col-md-4">
-                            <span class="leder_cirlcle_rank_text">01<span>Rank</span></span>
-                        </div>
-
-                    </div>
-                    <div class="col-md-6 no-padding  area_heandel">
-                        <div class="pull-left">
-                            <span class="leader_curnt_text vcenter">CURRENT LEADER</span>
-                        </div>
-                        <div class="pull-right">
-                            <span class=""><img src={{URL::to('/img/leader_bord_cup.png')}} alt=""/></span>
-                        </div>
-                        <div class="clear clearfix"></div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="section_leader_secnd col-md-12">
-                <div class="col-md-6 col-sm-12 back_white_leader  pull-left">
-                    <div class="col-md-12 pull-left">
-                        <div class="col-md-4">
-                            <div class="cnter_liez">
-                                <img class="img-responsive "  src="
-                              @if("http://www.gamithonfantasy.com/assets-new/img/default-profile-pic.png"==getUploadsPath($leaders[1]['user']['profile_pic']))
-                                {{getUploadsPath($leaders[1]['user']['profile_pic'])}}
-                                @else
-                                <?php
-                                $check=explode(".",$leaders[1]['user']['profile_pic']);
-                                if($check[1]=="facebook"){
-                                    echo $leaders[1]['user']['profile_pic'];
-                                }else{
-                                    echo Croppa::url(getUploadsPath($leaders[1]['user']['profile_pic']),156,134);
-                                }
-
-                                ?>
-                                @endif" alt=""/>
-                            </div>
-                            <div class="clear clearfix"></div>
-                        </div>
-                        <div class="col-md-4 text-center  area_leader">
-                            <span class="leder_cirlcle_jhon_text">{{$leaders[1]['user']['name']}}</span>
-                            <span class="leder_cirlcle_jhon_text_num">{{$leaders[1]['score']}}</span>
-                            <span class="leder_smaal_text">Current Points</span>
-                        </div>
-                        <div class="col-md-4">
-                            <span class="leder_cirlcle_rank_text">02<span>Rank</span></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 back_white_leader  pull-right">
-                    <div class="col-md-12">
-                        <div class="col-md-4">
-                            <div class="cnter_liez">
-                                <img class="img-responsive" src="
-                               @if("http://www.gamithonfantasy.com/assets-new/img/default-profile-pic.png"==getUploadsPath($leaders[2]['user']['profile_pic']))
-                                 {{getUploadsPath($leaders[2]['user']['profile_pic'])}}
-                                @else
-                                <?php
-                                $check=explode(".",$leaders[2]['user']['profile_pic']);
-                                if($check[1]=="facebook"){
-                                    echo $leaders[2]['user']['profile_pic'];
-                                }else{
-                                    echo Croppa::url(getUploadsPath($leaders[2]['user']['profile_pic']),156,134);
-                                }
-
-                                ?>
-                                @endif" alt=""/>
-                            </div>
-                            <div class="clear clearfix"></div>
-                        </div>
-                        <div class="col-md-4 text-center  area_leader">
-                            <span class="leder_cirlcle_jhon_text">{{$leaders[2]['user']['name']}}</span>
-                            <span class="leder_cirlcle_jhon_text_num">{{$leaders[2]['score']}}</span>
-                            <span class="leder_smaal_text">Current Points</span>
-                        </div>
-
-                        <div class="col-md-4">
-                            <span class="leder_cirlcle_rank_text">03<span>Rank</span></span>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-md-12">
-                <?php $i=0?>
-                @foreach($leaders as $key=>$row)
-
-                    <?php if($i>=3){?>
-                    <div class="col-md-4 itemsz col-sm-12 leader_small_sec no-padding">
-                        <div class="col-md-3 ">
-                            <div class="cnter_liez1">
-                                <img class="img-responsive " src="{{getUploadsPath($row['user']['profile_pic'])}}"
-                                     alt=""/>
-                            </div>
-                            <div class="clear clearfix"></div>
-                        </div>
-                        <div class="col-md-5 text-center  area_leader_second ">
-                            <span class="leder_cirlcle_jhon_text_second">{{$row['user']['name']}}</span>
-                            <span class="leder_cirlcle_jhon_text_num_second">{{$row['score']}}</span>
-                            <span class="leder_smaal_text_second">Current Points</span>
-                        </div>
-
-                        <div class="col-md-4 ">
-                            <span class="leder_cirlcle_rank_text_second">
-
-                               <?php if(checksingledigit($key+1)) echo '0';?>{{$key+1}}<span>Rank</span></span>
-                        </div>
-
-                    </div>
-                    <?php }
-                    $i++?>
-                @endforeach
-
-            </div>
-       @endif
-=======
                                          @endif" alt=""/>
                                 </div>
                                 <div class="clear clearfix"></div>
@@ -649,7 +488,6 @@
 
                 </div>
             @endif
->>>>>>> develop
 
         </div>
     </div>

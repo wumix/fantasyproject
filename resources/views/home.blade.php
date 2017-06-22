548a8d1647b63e628725ae82f2f724d98764449a
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-22 14:48:30 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -523,6 +523,7 @@
 
 
 
+            {url: '{{URL::to('assets-new/img/gamithon--22-j-banner.jpg')}}', fade: 500},
             {url: '{{URL::to('assets-new/img/champions-trophy-pakistan.jpg')}}', fade: 500},
 
 

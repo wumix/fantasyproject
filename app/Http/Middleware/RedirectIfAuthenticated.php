e8a9d21b2b7244928138baa884fc5a2397e74d55
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-22 10:52:01 +0500

    merge

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,7 +20,7 @@ class RedirectIfAuthenticated {
 
         if (Auth::guard($guard)->check()) {
 
-           //return redirect('\dashboard');
+           return redirect('\dashboard');
         }
 
         return $next($request);

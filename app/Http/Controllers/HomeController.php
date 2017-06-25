82abe5d60f8b05a649d70d296f422cd6b240b8c2
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-06-25 22:29:41 +0500

    merge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -83,7 +83,8 @@ class HomeController extends Controller
     public function index()
     {
 
-        $objTourmament = \App\Tournament::all()->sortBy("start_date")->where('start_date', '<=', getGmtTime())->Where('end_date', '>=', getGmtTime());
+        $objTourmament = \App\Tournament::all()->sortBy("start_date")->
+        where('start_date', '<=', getGmtTime())->Where('end_date', '>=', getGmtTime());
         $data['tournaments_list'] = $objTourmament->toArray(); //list of active
         //dd($data['tournaments_list']);
       //  dd($tournaments_list);
@@ -99,7 +100,7 @@ class HomeController extends Controller
        // $data['tournaments_list']['leaderboard']=\App\Leaderboard::where('tournament_id', config('const.tournament_id'))->with('user', 'user_team')->take(3)->orderBy('score', 'DESC')->get()->toArray();
         $upcommingTour = \App\Tournament::all()->sortBy("start_date")->where('start_date', '>=', getGmtTime());
         $data['upcomming_tournaments_list'] = $upcommingTour->toArray(); //upcomming tournament of active
-        $data['matches'] = \App\Match::getNextMatch();
+       // $data['matches'] = \App\Match::getNextMatch();
        //$data['matches']=$data['matches']->toarray();
        // $data['leaders'] = \App\Leaderboard::where('tournament_id', config('const.tournament_id'))->with('user', 'user_team')->take(3)->orderBy('score', 'DESC')->get()->toArray();
         $data['news'] = \App\BlogPost::where('post_type', 'news')->take(3)->orderBy('id', 'DESC')->get()->toArray();

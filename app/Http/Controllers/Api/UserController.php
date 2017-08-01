75275abb0b30b8bd6d5d48c6c6fb1b03728710a5
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-01 11:14:28 +0500

    merge

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -436,7 +436,7 @@ class UserController extends Controller
     }
  function isTournamentActive($tournament_id){
      $objTourmament=\App\Tournament::find($tournament_id)->
-     where('start_date', '>=', getGmtTime())->Where('end_date', '>=', getGmtTime())->first();
+     where('start_date', '<=', getGmtTime())->Where('end_date', '>=', getGmtTime())->first();
      //list of active
       //dd($objTourmament);
      if(empty($objTourmament)){

79aea9713b8227b2cec23b5ad63912572a22ae99
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-02 16:25:16 +0500

    api

---
+++ app/Http/Requests/CreateTeamRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'id' => 'required',
+        ];
+    }
+
+    public function response(array $errors)
+    {
+        $transformed = [];
+
+        foreach ($errors as $field => $message) {
+            $transformed[] = [
+                'field' => $field,
+                'message' => $message[0]
+            ];
+        }
+
+        return response()->json([
+            'errors' => $transformed
+        ], \Illuminate\Http\JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}

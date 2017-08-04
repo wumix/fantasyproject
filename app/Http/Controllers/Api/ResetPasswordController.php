c108debed4f93ec7504a31980f95df0b216b4c59
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-04 15:44:08 +0500

    forgot password

--- app/Http/Controllers/Api/ResetPasswordController.php
+++ app/Http/Controllers/Api/ResetPasswordController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Transformers\Json;
 use Illuminate\Foundation\Auth\ResetsPasswords;

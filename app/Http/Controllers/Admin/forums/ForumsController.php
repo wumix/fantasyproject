6c525f69608c1ea394e0e20a6cd0a4c0cbe7f545
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-08 07:05:41 +0500

    forums

---
+++ app/Http/Controllers/Admin/forums/ForumsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Forums;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Validator;
+
+class ForumsController extends Controller
+{
+    function index(){
+        //  $data['lists'] = \App\ForumCategory::where('is_approved',0)->with('children')->paginate(10);
+        $data['lists'] = \App\ForumCategory::where('parent_id','!=',NULL)->with('children')->paginate(10);
+        return view('adminlte::forums.home', $data);
+    }
+    function addCategory(Request $request){
+
+        return view('adminlte::forums.category');
+    }
+    function approve(Request $request){
+        //dd($request->all());
+
+        $approve=\App\ForumCategory::where('id',$request->post_id);
+        $approve->update(['is_approved'=>$request->is_approved]);
+
+
+
+
+    }
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+
+
+            'name' => 'required|unique:forum_categories',
+
+        ]);
+    }
+    function addCategoryPost(Request $request){
+        // dd($request->all());
+        // if($this->validator($request->all())->validate()) {
+        if(1){
+            $cat = new \App\ForumCategory;
+            $cat->name = $request->name;
+            $cat->is_approved = 1;
+            $cat->slug = slugify($request->name);
+            $cat->description = $request->description;
+            $cat->created_at = getGmtTime();
+            $cat->save();
+            return redirect()->back()->with('status', 'Caategory Added');
+        }else{
+            return redirect()->back()->with('status', 'Caategory Added');
+        }
+    }
+    public  function listcategory(){
+        $data['lists'] = \App\ForumCategory::where('parent_id',NULL)->with('children')->paginate(10);
+        return view('adminlte::forums.list_cat', $data);
+    }
+    public function editCategory($id){
+        $data['category'] = \App\ForumCategory::where('id',$id)->firstOrFail()->toArray();
+        //   dd( $data['category'] );
+        return view('adminlte::forums.category_edit',$data);
+    }
+    public function postEditCategory($id,Request $request){
+
+        $request->request->remove('_token');
+        $request->request->add(['created_at'=>getGmtTime()]);
+        $edit_cat = new \App\ForumCategory;
+        $edit_cat->where('id',$id)->update($request->all());
+        return redirect()->back();
+
+//        return view('adminlte::forums.category_edit',$data);
+    }
+
+}

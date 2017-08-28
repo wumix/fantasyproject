75bee4b73e685ddc700a0d61d10d57cb03709f6f
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-28 12:53:13 +0500

    stats

--- app/Http/Controllers/Admin/MatchesController.php
+++ app/Http/Controllers/Admin/MatchesController.php
@@ -39,7 +39,7 @@ class MatchesController extends Controller {
         if(empty($teams)){
             abort(404);
         }
-       $data['teams']=$teams;
+        $data['teams']=$teams;
         $data['matchid']=$matchid;
         $data['tournament_id']=$tournament_id;
         return view('adminlte::teams.add_team_to_match',$data); //shows add team to match form
@@ -49,25 +49,25 @@ class MatchesController extends Controller {
 
     }
     function addTeamToMatchPost(Request $request,$match_id,$tournament_id){
-   if(count($request->teams_id)!=2) {
-       return redirect()
-           ->route('addTeamToMatch', ['match_id' => $match_id,'tournament_id'=>$tournament_id])
-           ->with('status', 'You must choose only two teams');
-   }
+        if(count($request->teams_id)!=2) {
+            return redirect()
+                ->route('addTeamToMatch', ['match_id' => $match_id,'tournament_id'=>$tournament_id])
+                ->with('status', 'You must choose only two teams');
+        }
 
-   $myplayers=[];
-   foreach($request->teams_id as $row) {
-       $players = \App\Team::where('id', $row)->with('team_players')->firstOrFail()->toArray();
+        $myplayers=[];
+        foreach($request->teams_id as $row) {
+            $players = \App\Team::where('id', $row)->with('team_players')->firstOrFail()->toArray();
 
-foreach ($players['team_players'] as $key=>$val){
-    $myplayers[]=$val['id'];
-
-
-}
+            foreach ($players['team_players'] as $key=>$val){
+                $myplayers[]=$val['id'];
 
 
+            }
 
-   }
+
+
+        }
         $objMatch = \App\Match::findORFail($match_id);
         $objMatch->match_players()->sync(array_filter($myplayers));
         return redirect()
@@ -75,12 +75,21 @@ foreach ($players['team_players'] as $key=>$val){
             ->with('status', 'Teams Added sucessfully');
     }
     public function addMatch(Request $request) {
-        //dd($request->all());
+
+
         $this->objMatch->fill($request->all());
+        if($request->hasFile('team_1_logo')){
+            $files = uploadInputs($request->file('team_1_logo'), 'tournament_logos');
+            $this->objMatch->team_1_logo=$files;
+        }
+        if($request->hasFile('team_2_logo')){
+            $files = uploadInputs($request->file('team_2_logo'), 'tournament_logos');
+            $this->objMatch->team_2_logo=$files;
+        }
         $this->objMatch->save();
         return redirect()
-                        ->route('editMatchForm', ['match_id' => $this->objMatch->id])
-                        ->with('status', 'Match Saved');
+            ->route('editMatchForm', ['match_id' => $this->objMatch->id])
+            ->with('status', 'Match Saved');
     }
 
     function editMatchForm($match_id) {
@@ -100,19 +109,17 @@ foreach ($players['team_players'] as $key=>$val){
     }
 
     function postEditMatch($match_id, Request $request) {
-
         $match = \App\Match::find($match_id);
         $match->fill($request->all());
         if($request->hasFile('team_1_logo')){
             $files = uploadInputs($request->file('team_1_logo'), 'tournament_logos');
-            $match->team_1_logo=$files;
+            $this->objMatch->team_1_logo=$files;
         }
         if($request->hasFile('team_2_logo')){
             $files = uploadInputs($request->file('team_2_logo'), 'tournament_logos');
-            $match->team_2_logo=$files;
+            $this->objMatch->team_2_logo=$files;
         }
         $match->save();
-       // dd($request->all());
         return redirect()->route('editMatchForm', ['match_id' => $match_id])->with('status', 'Match Updated');
     }
 
@@ -128,6 +135,7 @@ foreach ($players['team_players'] as $key=>$val){
     }
 
     function postAddMatchPlayers($match_id, Request $request) {
+        dd($request->all());
         $objMatch = \App\Match::findORFail($match_id);
         $objMatch->match_players()->sync(array_filter($request->player));
         return redirect()->back()->with('status', 'Match player score updated.');
@@ -135,31 +143,39 @@ foreach ($players['team_players'] as $key=>$val){
 
     function getMatchPlayers($match_id) {
         $data['players'] = Match::where('id', $match_id)
-                ->with('match_players')
-                ->firstOrFail()
-                ->toArray();
+            ->with('match_players')
+            ->firstOrFail()
+            ->toArray();
         //dd($data);
         return view('adminlte::matches.show_players', $data);
     }
 
     function playerMatchScore($match_id, $player_id) {
         $gameId = Match::where('id', $match_id)->with('match_tournament')->firstOrFail()
-                ->toArray();
+            ->toArray();
         $gameId = $gameId['match_tournament']['game_id'];
 
         $data['player'] = Player::where('id', $player_id)->first()->toArray();
         $data['match'] = Match::where('id', $match_id)->with([
-                    'player_scores' => function($query) use ($player_id) {
-                        $query->where('player_id', $player_id);
-                    }
-                ])->first()->toArray();
+            'player_scores' => function($query) use ($player_id) {
+                $query->where('player_id', $player_id);
+            }
+        ])->first()->toArray();
 
         $data['game_actions'] = \App\GameAction::where('game_id', $gameId)
-                        ->with('game_terms')
-                        ->get()->toArray();
+            ->with('game_terms')
+            ->get()->toArray();
         $data['match_id'] = $match_id;
+        $data['player_stat']=\App\PlayerMatchStats::where('match_id', $match_id)
+            ->where('player_id', $player_id)->first();
+        if(!empty($data['player_stat'])){
+            $data['player_stat']=$data['player_stat']->text;
+        }else{
+            $data['player_stat']="N-A";
+        }
 
-        //dd($data);
+
+
 
         return view('adminlte::matches.add_player_match_score', $data);
     }
@@ -167,8 +183,8 @@ foreach ($players['team_players'] as $key=>$val){
     function postAddMatchScore($match_id, $player_id) {
 
         \App\MatchPlayerScore::where('match_id', $match_id)
-                ->where('player_id', $player_id)
-                ->delete();
+            ->where('player_id', $player_id)
+            ->delete();
         foreach (Input::get('term_score') as $key => $val) {
             $playerMatchPoints['match_id'] = $match_id;
             $playerMatchPoints['player_id'] = $player_id;
@@ -176,6 +192,19 @@ foreach ($players['team_players'] as $key=>$val){
             $playerMatchPoints['player_term_count'] = empty($val) ? 0 : $val;
             \App\MatchPlayerScore::insert($playerMatchPoints);
         }
+        return redirect()->back()->with('status', 'Match player score updated.');
+    }
+    function postAddMatchStats(Request $request,$match_id, $player_id) {
+
+        \App\PlayerMatchStats::where('match_id', $match_id)
+            ->where('player_id', $player_id)
+            ->delete();
+        $playerMatchPoints['text']=$request->details;
+        $playerMatchPoints['match_id'] = $match_id;
+        $playerMatchPoints['player_id'] = $player_id;
+        \App\PlayerMatchStats::insert($playerMatchPoints);
+
+
         return redirect()->back()->with('status', 'Match player score updated.');
     }
 

a2a5a9ba6c8ae9306cfab4fd0d43f5cf709feb74
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-08-29 00:54:12 +0500

    merge branches

--- resources/views/user/team_detail1.blade.php
+++ resources/views/user/team_detail1.blade.php
@@ -191,11 +191,7 @@
                         <a href="#" class="gt_teams">{{$match['team_two']}}</a>
                         <?php
                         if (empty($match['result'])) {
-<<<<<<< HEAD
-                            $match['result'] = "To Be Decided";
-=======
                             $match['result'] = "";
->>>>>>> master
                         }
                         ?>
                         <span class="gt_match_result">{{$match['result']}}</span>
@@ -204,43 +200,6 @@
                     <div class="table-responsive gt_scorecard_wrapper">
                         <!--<table class="table table-striped">-->
                         @if(!empty($match['match_players']))
-<<<<<<< HEAD
-                        <table class="table">
-                            <thead>
-                            <tr class="grean_bg_row">
-                                <th>Score Card</th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th colspan="2">
-                                    <div class="gt_team_select">
-                                        Select Team:
-                                        <select id="gt_team_select" class="form-control"
-                                                style="width: 50%; display: inline-block;">
-                                            <option
-                                                    @if($team_name==$match['team_one'])
-                                                    selected
-                                                    @endif
-                                                    value="{{$match['team_one']}}">{{$match['team_one']}}</option>
-                                            <option @if($team_name==$match['team_two'])
-                                                    selected
-                                                    @endif value="{{$match['team_two']}}">{{$match['team_two']}}</option>
-                                        </select>
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr class="grey_bg_row">
-                                <th>PLAYERS</th>
-                                <th></th>
-                                <th></th>
-                                <th>R</th>
-                                <th>6s</th>
-                                <th>4s</th>
-=======
                             <table class="table">
                                 <thead>
                                 <tr class="grean_bg_row">
@@ -305,9 +264,9 @@
                                                 @if($player['player_roles'][0]['pivot']['game_role_id']==7)
                                                     <img src="{{URL::to('/img/all-rounder-logo.png')}}" alt="">
                                                 @endif
-                                                    @if($player['player_roles'][0]['pivot']['game_role_id']==8)
-                                                        <img src="{{URL::to('/img/batsman-logo.png')}}" alt="">
-                                                    @endif
+                                                @if($player['player_roles'][0]['pivot']['game_role_id']==8)
+                                                    <img src="{{URL::to('/img/batsman-logo.png')}}" alt="">
+                                                @endif
                                             </td>
                                             <td><?php
                                                 $text = "N-A";
@@ -336,70 +295,9 @@
                                             <td></td>
                                         </tr>
                                     @endif
->>>>>>> master
-
-
-                                <th>SR</th>
-                                <th>ECON</th>
-
-<<<<<<< HEAD
-                                <th>W</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-
-                            @foreach($match['match_players'] as $player)
-                                @if(strtoupper($player['player_actual_teams'][0]['name'])==$team_name)
-                                    <tr>
-                                        <td>{{$player['name']}}</td>
-
-
-                                        <td><img src="{{URL::to('/img/all-rounder-logo.png')}}" alt=""></td>
-                                        <td><?php
-                                            $text = "N-A";
-                                            if (!empty($player['player_match_stats'][0]['text'])) {
-                                                $text = $player['player_match_stats'][0]['text'];
-                                            }
-                                            echo $text;
-                                            ?></td>
-                                        <?php $temp = ['Runs' => '-', '6s' => '-', '4s' => '-', 'Scoring Rate' => '-', 'R.R.P.O' => '-',
-                                            'Wicket Taken' => '-'];
-
-                                        ?>
-
-
-                                        @foreach($temp as $key=>$terms)
-
-                                            @if($t=zulfi_in_array($key,$player['player_game_term_score']))
-
-                                                <td>{{$t}}</td>
-                                            @else
-                                                <td>-</td>
-                                            @endif
-
-                                        @endforeach
-
-
-                                    </tr>
-                                @endif
 
 
 
-                            @endforeach
-
-                            </tbody>
-                        </table>
-                        @else
-                            No Records
-                        @endif
-                    </div>
-
-
-                </div>
-
-=======
                                 @endforeach
 
                                 </tbody>
@@ -412,7 +310,6 @@
 
                 </div>
 
->>>>>>> master
             </div>
 
         </div>
@@ -422,11 +319,7 @@
 @section('js')
     <script>
         $('#gt_team_select').change(function () {
-<<<<<<< HEAD
-            window.location = "{{route('scorecard',['id'=>$match['id']])}}?team_name=" + $(this).val();
-=======
             window.location = "{{route('scorecard',['id'=>$match['id'],'tournament_id'=>$tournament_id])}}?team_name=" + $(this).val();
->>>>>>> master
         });
     </script>
 

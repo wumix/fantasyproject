bd746c63ccf88585485b4ae7ef898ca395437c89
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-14 16:22:59 +0500

    compser json

---
+++ resources/views/pages/about_us.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+<section>
+    @section('title')
+        Privacy Policy
+    @stop
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-heading">
+                    Privacy policy
+                </h1>
+                <hr class="light full">
+                <div class="page-content">
+
+                    <p>The Privacy Policy of Gamithon Fantasy includes all the actors and characters involved in this portal as fictitious and not actual. Gamithon Fantasy collects delicate information from users and discloses only the information that is required for the game available on the web portal: www.gamithonfantasy.com. Other than the required information, all the rest are kept in extreme secrecy &amp; privacy.</p>
+                    <strong>Users Personal Information</strong>
+                    <p>Gamithon Fantasy required user&rsquo;s personal information from every user. User has been asked for accurate Name, email address and other personal information voluntarily. Users may also refuse to provide personal information unless it affects the game ativity.</p>
+                    <p>If users have any queries about how to register on the website and for other activities, we have made it available in the website.</p>
+                    <strong>Users Non-personal Information</strong>
+                    <p>Non-personal information may also require by Gamithon Fantasy e.g. the type of computer browser name, computer name and connection medium to our site.</p>
+                    <strong>Web browser cookies</strong>
+                    <p>We placed a little text file called a cookie on your hard drive that lets us store your profile information. Placing a cookie is just for record keeping and to track information about user when required .if user is not willing for these cookies he may set to choose their web browser to refuse cookies.</p>
+                    <strong>How we use collected information</strong>
+                    <p>For the following purposes Gamithon Fantasy collect and use user personal information.</p>
+                    <ul>
+                        <li>Customer services improvement</li>
+                        <li>To support users need more efficiently</li>
+                        <li>To run a promotion, contest, survey or other Site feature</li>
+                        <li>We may use the email address provided by the user to send them periodic emails about their inquires, questions and other request.<br /> if user would like to unsubscribe from future emails he may can at any time.</li>
+                    </ul>
+                   <strong>How we protect your information</strong>
+                    <p>Gamithon Fantasy do not Allow any unauthorized access to your personal identification information e.g. username, password and all kind of users data. We keep users data in secrecy and Privacy.</p>
+                    <strong>Sharing your personal information</strong>
+                    <p>Gamithon Fantasy do not share and sell users personal identity information. we may share users generic information not associated with your personal identification with our business partners and trust worthies. We may share your personal information for limited purposes to third parties on our behalf.</p>
+                    <strong>Compliance with children's online privacy protection act</strong>
+                    <p>Our website is not designed to attract anyone under 12.for this purpose, children under 12 are strictly prohibited for the website.</p>
+
+                    <strong>Your acceptance of these terms</strong>
+                    <p>By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site</p>
+                    <strong>Contacting us</strong>
+                    <p>If you have any questions about our Privacy Policy, the practices of this site, or your dealings with this site, please contact us at: info@gamithonfantasy.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@stop

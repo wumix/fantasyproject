ec3ac2740c0d33778e980622434354c73ad2faed
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-20 11:32:14 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -464,6 +464,56 @@
         <div class="container-fluid">
             <div class="row no-gutter popup-gallery">
                 <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/301.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/301.jpg" class="img-responsive" alt=" DD VS GL">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    England vs West Indies
+                                </div>
+                                <div class="project-name">
+                                    England won by 7 wickets
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/302.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/302.jpg" class="img-responsive" alt="KXIP VS MI">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        England vs West Indies
+                                    </div>
+                                    <div class="project-name">
+                                        England won by 7 wickets
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="img/portfolio/thumbnails/303.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/303.jpg" class="img-responsive" alt=" DD VS GL">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="portfolio-box-caption-content">
+                                    <div class="project-category text-faded">
+                                        England vs West Indies
+                                    </div>
+                                    <div class="project-name">
+                                        England won by 7 wickets
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/thumbnails/101.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/101.jpg" class="img-responsive" alt=" DD VS GL">
                         <div class="portfolio-box-caption">
@@ -555,51 +605,6 @@
                                 </div>
                                 <div class="project-name">
                                     West Indies won by 21 runs
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/thumbnails/601.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/601.jpg" class="img-responsive" alt=" DD VS GL">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Barbados Tridents Vs Knight Riders
-                                </div>
-                                <div class="project-name">
-                                    Patriots won by 10 wickets
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/thumbnails/602.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/602.jpg" class="img-responsive" alt="KXIP VS MI">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Barbados Tridents Vs Knight Riders
-                                </div>
-                                <div class="project-name">
-                                    Patriots won by 10 wickets
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/thumbnails/603.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/603.jpg" class="img-responsive" alt=" DD VS GL">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Barbados Tridents Vs Knight Riders
-                                </div>
-                                <div class="project-name">
-                                    Patriots won by 10 wickets
                                 </div>
                             </div>
                         </div>

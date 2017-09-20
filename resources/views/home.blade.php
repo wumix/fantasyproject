282fa88f9c4a64cbd33b72d29419b4dd8e7b6433
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-20 11:22:54 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -356,7 +356,7 @@
                                     </h4>
 
                                     <span style="font-size: 12px; min-height:85px;display:block" >
-                       {!! substr($val['description'],0, 100) !!}...
+                       {!! substr($val['description'],0, 130) !!}...
                             <br>
 
     </span>

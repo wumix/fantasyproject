82e7e8dea19b4845fa279ae5f63a58cd3664762c
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-20 21:51:02 +0500

    push

---
+++ app/UserChallengeTeamStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserChallengeTeamStatus extends Model
+{
+    protected $table = 'user_challenge_team_status';
+
+    public $timestamps = false;
+    protected $guarded = [];
+}

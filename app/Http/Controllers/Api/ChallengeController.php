cbcf374416a4e61bbb9f9b97507050c7713e850b
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-27 16:16:34 +0500

    balakia photo

--- app/Http/Controllers/Api/ChallengeController.php
+++ app/Http/Controllers/Api/ChallengeController.php
@@ -39,8 +39,9 @@ class ChallengeController extends Controller
         $user_1_id = \Auth::Id();
         $request->request->add(['user_1_id'=>$user_1_id]);
         if (empty($match_id)) {
-            return redirect()->back()
-                ->with('status', 'You have must select a match');
+            $data['status']=false;
+            $data['message']="Please select a match";
+
         }
         $data = $this->userChallenge->where(['user_1_id' => $user_1_id, 'user_2_id' => $request->user_2_id])->first();
         // if (empty($data)) {
@@ -63,13 +64,13 @@ class ChallengeController extends Controller
                     'is_complete' => 0,
                 ]);
             \Mail::to($reciever->email)->send(new \App\Mail\Challenge($sender->name, $reciever->name));
-            return redirect()->route('addChallengeTeam',
-                [
-                    'match_id' => $match_id,
-                    'challlenge_id' => $this->userChallenge->id
-                ]);
+            $data['status']=true;
+            $data['message']="Challenge sent successfully";
+            return response()->json($data);
 
         } else {
+            $data['status']=true;
+            $data['message']="";
             return redirect()->back()
                 ->with('status', 'You have already challenged this user');
         }

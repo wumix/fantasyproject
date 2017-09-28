452287fac2cdb7e20b779f663285532b4e285a75
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-28 12:19:18 +0500

    about page

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -12,16 +12,15 @@
                 </h1>
                 <hr class="light full">
                 <div class="page-content">
-                    <p>Fantasy cricket is a strategy game, where users rely on their selection skills to pick their dream team. Thus it is considered legal (not gambling) in US and most part of the world.
-                        Based on their cricketers’ real-life performance in the following match, users get points and compete with others to win prizes.  Our users could still enjoy the whole experience of registering, creating their fantasy cricket team and playing in leagues completely for free, but they now had to use their skills to win cash leagues.
-                    </p>
-                    <p>
-                        Own your own team, Play your own Game.
-                    </p>
+                   <p> Fantasy cricket is a strategy game, where users rely on their selection skills to pick their dream team. Thus it is considered legal (not gambling) in US and most part of the world.
+                    Based on their cricketer's real-life performance in the following match, users get points and compete with others to win prizes.  Our users could still enjoy the whole experience of registering, creating their fantasy cricket team and playing in tournaments/series completely free, but now they have to use their skills to win cash prizes.
+                   </p>
+                    <p>Own your own team, Play your own Game.</p>
 
-                        Good luck,
-                        Team Gamithon
-                    </p>
+
+                    Good luck,
+                    Team Gamithon
+
                 </div>
             </div>
         </div>

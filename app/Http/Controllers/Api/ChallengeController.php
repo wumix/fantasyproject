6820a03a615d96b08d8a2bcec68a576493597272
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-09-29 11:36:57 +0500

    challenge api

--- app/Http/Controllers/Api/ChallengeController.php
+++ app/Http/Controllers/Api/ChallengeController.php
@@ -25,11 +25,11 @@ class ChallengeController extends Controller
     {
 
         $data['sent_challenges'] = \App\UserChallenge::where(['user_1_id' => \Auth::id()])->with(['user_by'])->get()->toArray();
-        $accepted_challenges= \App\User::where(['id' => \Auth::id()])->with(['challenges' => function ($query) {
-            //$query->where('status', 1);
+        $accepted_challenges = \App\User::where(['id' => \Auth::id()])->with(['challenges' => function ($query) {
+            $query->where('status', 1);
         }, 'challenges.user'])->get()->toArray();
-//dd($accepted_challenges);
-        $data['accepted_challenges']=$accepted_challenges[0]['challenges'];
+        dd($accepted_challenges);
+        $data['accepted_challenges'] = $accepted_challenges[0]['challenges'];
         return response()->json($data);
     }
 
@@ -38,10 +38,10 @@ class ChallengeController extends Controller
 
         $match_id = $request->match_id;
         $user_1_id = \Auth::Id();
-        $request->request->add(['user_1_id'=>$user_1_id]);
+        $request->request->add(['user_1_id' => $user_1_id]);
         if (empty($match_id)) {
-            $data['status']=false;
-            $data['message']="Please select a match";
+            $data['status'] = false;
+            $data['message'] = "Please select a match";
 
         }
         $data = $this->userChallenge->where(['user_1_id' => $user_1_id, 'user_2_id' => $request->user_2_id])->first();
@@ -65,22 +65,24 @@ class ChallengeController extends Controller
                     'is_complete' => 0,
                 ]);
             \Mail::to($reciever->email)->send(new \App\Mail\Challenge($sender->name, $reciever->name));
-            $data['status']=true;
-            $data['message']="Challenge sent successfully";
+            $data['status'] = true;
+            $data['message'] = "Challenge sent successfully";
             return response()->json($data);
 
         } else {
-            $data['status']=true;
-            $data['message']="";
+            $data['status'] = true;
+            $data['message'] = "";
             return redirect()->back()
                 ->with('status', 'You have already challenged this user');
         }
     }
-    function challengeTeam(Request $request){
 
-        $challenge_id=$request->challenge_id;
+    function challengeTeam(Request $request)
+    {
+
+        $challenge_id = $request->challenge_id;
         $data['challenge_id'] = $challenge_id;
-        $match_id= \App\UserChallenge::where('id',$challenge_id)->first()->match_id;
+        $match_id = \App\UserChallenge::where('id', $challenge_id)->first()->match_id;
         if (challengeTeamCompleteInChallenge(\Auth::id(), $challenge_id)) {
             return redirect()->route('UserDashboard')->with('status', 'Compeleted');
         }

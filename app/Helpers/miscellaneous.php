c82cb2653cea3e289a72f7def1e6cbcc0b2f681c
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-10-02 17:31:48 +0500

    challenge

--- app/Helpers/miscellaneous.php
+++ app/Helpers/miscellaneous.php
@@ -16,12 +16,14 @@ function membershipFeaturePoints($userid,$feature)
     }
     return $points;
 }
-function challengeTeamCompleteInChallenge($user_id,$challenge_id){
+function challengeTeamCompleteInChallenge($user_id=450,$challenge_id=5){
+    
     $is_complete=\App\UserChallengeTeamStatus::where('user_id', $user_id)->
     where('challenge_id', $challenge_id)->first();
     if(!empty($is_complete)){
-        $is_complete->is_complete;
+        $is_complete=$is_complete->is_complete;
     }
+
     if($is_complete==1){
         return true;
     }else{

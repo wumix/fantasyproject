bb97876ed0314dc8a98763e1cc88a26b3e6cf5b4
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-10-10 17:51:37 +0500

    live score

--- app/Http/Controllers/Api/LiveScoreController.php
+++ app/Http/Controllers/Api/LiveScoreController.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\Controller;
 class LiveScoreController extends Controller
 {
     function index(){
-        $data['match_scores']=\App\Match::where('cricscore_api','!=',0)
+        $data['scores']=\App\Match::where('cricscore_api','!=',0)
             ->with('match_scores')->get()->toArray();
-       return response()->json($data['match_scores']);
+       return response()->json($data);
     }
 }

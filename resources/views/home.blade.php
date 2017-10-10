b6ec105a88cfd1a61ad75b0f45dc7ec3eb186dba
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-10-10 23:27:46 +0500

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -891,6 +891,6 @@ Cricket app">
     <meta property="og:description" content="Join referral here"/>
     <meta property="og:image:width" content="1200"/>
     <meta property="og:image:height" content="600"/>
-    <meta property="og:image" content="{{URL::to('assets-new/img/default-profile-pic-1.png')}}"/>
+    <meta property="og:image" content="{{URL::to('/img/fb_share_logo.png')}}"/>
     <meta property="fb:app_id" content="337419313358697"/>
 @stop

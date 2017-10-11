e26144d04d7f2988b124f6a0c59a7583237c7c97
zulfiqar tariq <zulfiqar_tariq@hotmail.com>
2017-10-11 13:35:55 +0500

    scoredcard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,7 +96,7 @@ Cricket app">
 
                 <!-- Fixture Start -->
                 @foreach($match_scores as $match_score)
-                <a
+                <a class="colorbox"
                    href="{{route('scorecardpopup',['slug'=>$match_score['id'],'tournament_id'=>$match_score['tournament_id']])}}"
                    class="view_scorecard_btn">
                     <div class="col-md-3 fixture_wrapper">
